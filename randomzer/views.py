28f8b5eb564469aa83623cfaca30b16d3b011e81
João Paulo Carnellossi <joao31554@gmail.com>
2024-07-09 17:45:25 -0300

    v1 cards fort

--- randomzer/views.py
+++ randomzer/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+import random
+
+fortuneList = [
+    "A vida trará coisas boas se tiver paciência.",
+    "Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.",
+    "Não compense na ira o que lhe falta na razão.",
+    "Defeitos e virtudes são apenas dois lados da mesma moeda.",
+    "A maior de todas as torres começa no solo."
+]
 
 # Create your views here.
 def tune(request):
-    context = {}
-    return render(request, 'randomizer/tune.html', context)
+    context = { 'fortune': random.choice(fortuneList)}
+    return render(request, 'randomzer/tune.html', context)
